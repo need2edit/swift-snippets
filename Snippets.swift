c42884b6f492463ec136b5aa9b0fcbb44a250692
Jake Young <need2edit@gmail.com>
2015-01-19 16:19:17 -0500

    Made extensions, functions public

--- Snippets.swift
+++ Snippets.swift
@@ -14,3 +14,52 @@
 
 import Foundation
 import UIKit
+
+// MARK: -- String Extensions
+
+// Quickly replace a string within a string
+// Credit: Stack Overflow - 24200888
+
+public extension String {
+    
+    // === Example Usage ===
+    
+    //  let string = "How cool is Swift?"
+    //  let updatedString = string.replace("cool", withString: "awesome")
+    
+    func replace(target: String, withString: String) -> String {
+        return self.stringByReplacingOccurrencesOfString(target, withString: withString, options: NSStringCompareOptions.LiteralSearch, range: nil)
+    }
+    
+    // Further adapted 24200888 to create string removal
+    
+    // === Example Usage ===
+    
+    //  let string = "How cool is Swift?"
+    //  let updatedString = string.replace("cool", withString: "awesome")
+    
+    func remove(stringToRemove: String) -> String {
+        return self.replace(target: stringToRemove, withString: "")
+    }
+}
+
+// MARK: -- Grand Central Dispatch
+
+// Delay a main queue dispatch by seconds and provide a closure
+// Credit: Stack Overflow - 24034544
+
+// === Example Usage ===
+
+//  delay(0.5) {
+//      doSomething()
+//  }
+
+public func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
+
