a1c94c751e3e182be5bf3273c52c96c73f7dbc43
Jake Young <need2edit@gmail.com>
2015-01-19 16:14:26 -0500

    First round of snippets
    Adds GCD and String Extension snippets

---
+++ Snippets.swift
@@ -0,0 +1,16 @@
+//
+//  Snippets.swift
+//  
+//
+//  Created by Jake Young on 1/19/15.
+//  Website: http://www.thoughtsfromarobot.com
+//
+//  These snippets have been collected or developed
+//  over my timeline of learning Swift, I did my best 
+//  to credit the original creators, I hope you find them
+//  useful and helpful.  For more resources, please visit
+//  my website at http://www.thoughtsfromarobot.com
+//
+
+import Foundation
+import UIKit
